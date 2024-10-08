665b89e54f53586d20b2527e0f929936447ffa0c
MarianLHG <hernandezmarian700@gmail.com>
2024-10-08 17:43:32 -0600

    fin practica3

--- intbootstrap1/resources/views/formulario.blade.php
+++ intbootstrap1/resources/views/formulario.blade.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(["resources/js/app.js"])
-    <link rel="stylesheet" href="{{ asset('css/fondo.css') }}">
-    <title>Formulario de registro</title>
-</head>
-<body>
-    <!-- Inicia navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('rutainicio') }}">Turista sin Maps</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Registro Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('rutaclientes') }}">Consulta Clientes</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- Finaliza navbar -->
+@extends('layouts.plantilla1')
+@section('titulo', 'Formulario')
+@section('contenido')
     <!-- inicia Tarjeta con formulario -->
     <div class="container mt-5 col-md-6">
         <div class="card font-monospace">
@@ -58,7 +31,4 @@
             <div class="card-footer text-muted">
                 <!-- Finaliza Tarjeta con formulario -->
             </div>
-        </div>
-    </div> 
-</body>
-</html>
+@endsection
