8598aff2a4f179cc43a608ca93a2c36fc24e2575
MarianLHG <hernandezmarian700@gmail.com>
2024-10-09 18:58:26 -0600

    practica 4

--- intbootstrap1/routes/web.php
+++ intbootstrap1/routes/web.php
@@ -2,14 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\controladoreVistas;
+Route::get('/',[controladoreVistas::class, 'home'])->name('rutainicio');
+Route::get('/form',[controladoreVistas::class, 'formulario'])->name('rutaformulario');
+Route::get('/clientes',[controladoreVistas::class, 'consulta'])->name('rutaclientes');
+Route::view('/componentes','componentes')->name(name: 'rutacomponentes');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //ruta de tipo GET
 /*Route::get('/', function () {
     return view('welcome');
 });*/
 
 //ruta de tipo view
-Route::view('/',view: 'inicio')->name('rutainicio');
+/* Route::view('/',view: 'inicio')->name('rutainicio');
 Route::view('/form','formulario')->name(name: 'rutaformulario');
 Route::view('/clientes','clientes')->name(name: 'rutaclientes');
 Route::view('/componentes','componentes')->name(name: 'rutacomponentes');
+ */
+
 
