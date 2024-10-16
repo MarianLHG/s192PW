4fb8b0da4d683c240d2ac71bd6d091717758f598
MarianLHG <hernandezmarian700@gmail.com>
2024-10-15 22:54:39 -0600

    Creación de la vista "inicio"

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+    <title>Portada</title>
+
+</head>
+<body>
+    <div class="container mt-5 col-md-3">
+    <div class="card" style="width: 18rem;">
+       <div class="card-body">
+         <h5 class="card-title">Marian Lizeth Hernández García</h5>
+         <p class="card-text">Carrera: Ingeniería en Sistemas</p>
+         <p class="card-text">Edad: 21 años</p>
+
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Grupo: S192</li>
+        <li class="list-group-item">Materia: Programación web</li>
+        <li class="list-group-item">Universidad Politécnica de Querétaro</li>
+    </ul>
+    </div> 
+    </div>
+</body>
+</html>
