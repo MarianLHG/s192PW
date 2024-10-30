f7698a1eae1e67d2dde5f5787d895a85fb5d7929
MarianLHG <hernandezmarian700@gmail.com>
2024-10-29 23:56:17 -0600

    Cambio de tipo de vistas view a get

--- repaso2/routes/web.php
+++ repaso2/routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\controladorRepaso2;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','inicio')->name('rutainicio');
-Route::view('/registro','registrolibro')->name('rutaregistro');
+Route::get('/',[controladorRepaso2::class,'inicio'])->name('rutainicio');
+Route::get('/registro',[controladorRepaso2::class,'rutaregistro'])->name('rutaregistro');
 Route::post('/enviarLibro', [controladorRepaso2::class,'guardarLibro'])->name('rutaenviar');
