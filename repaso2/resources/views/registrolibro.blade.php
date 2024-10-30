7d7de79a5a30bff176043d82c9373e80e23ba7fe
MarianLHG <hernandezmarian700@gmail.com>
2024-10-29 18:50:57 -0600

    modificacion del formulario de registrolibro

--- repaso2/resources/views/registrolibro.blade.php
+++ repaso2/resources/views/registrolibro.blade.php
@@ -1,38 +1,51 @@
  @extends('layouts.plantilla')
  @section('titulo', 'Registro')
  @section('contenido')
+    <div class="container mt-5 col-md-6">
+        <div class="card font-monospace">
+            <div class="card-header fs-5 text-center">
+                {{__('Registro Libro')}}
+            </div>
+            <div class="card-body text-justify">
 
     <form>
-    <div class="form-group">
+        @csrf
+
+    <div class="mb-3">
         <label for="formGroupExampleInput">ISBN</label>
         <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <label for="formGroupExampleInput2">Titulo</label>
         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <label for="formGroupExampleInput2">Autor</label>
         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <label for="formGroupExampleInput2">Paginas</label>
         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <label for="formGroupExampleInput2">Año</label>
         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <label for="formGroupExampleInput2">Editorial</label>
         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
     </div>
-    <div class="form-group">
+    <div class="mb-3">
         <label for="formGroupExampleInput2">Email de la editorial</label>
         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
     </div>
     </form>
- 
+    </div>
+    </div>
+    </div>
+    
+    <div class="mb-3">
+    </div>
  
  
  @endsection
