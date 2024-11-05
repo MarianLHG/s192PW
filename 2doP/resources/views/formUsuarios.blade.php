0a7e81cd5e669403e8976e1fefc3ed8b40c813cb
MarianLHG <hernandezmarian700@gmail.com>
2024-11-04 19:51:35 -0600

    1er commit

---
+++ 2doP/resources/views/formUsuarios.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/app.js'])
+</head>
+<body>
+
+    <h1 class="text-center text-primary mt-5 mb-4">Registro de usuarios</h1>
+
+    <div class="container  col-md-4">
+
+    <form>
+
+        <div class="mb-3">
+            <label  class="form-label">Correo: </label>
+            <input type="text" class="form-control"  name="correo">
+            
+
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Contraseña: </label>
+            <input type="text" class="form-control"  name="contraseña">
+        </div>                
+
+        <div class="mb-3">
+            <label  class="form-label">Edad: </label>
+            <input type="text" class="form-control" name="edad" >
+        </div>
+
+
+
+        <button type="" class="btn btn-danger "> Guardar Usuario </button>
+
+
+</div>
+
+
+</body>
+</html>
